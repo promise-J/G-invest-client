ca13ee0b41bcc4c3994455e79d5419b0a66f52fa
Promise Johnson <chiemelapromise30@gmail.com>
2023-03-29 22:25:08 +0100

    Writing hooks

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const useAuth = () => {
+   try {
+    console.log('use Auth is called')
+   } catch (error) {
+    console.log('use Auth has issues')
+   }
+}
+
+export default useAuth
